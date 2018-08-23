8d26a6e8d6a16e7fd2761cbf5d406467041e3bc1
Santoshkumar Yadav <syadav214@gmail.com>
2018-08-23 15:07:17 +0530

    created backend server for the blog site

---
+++ ReactJsApp/medium-reactjs-nodejs/server/app.js
@@ -0,0 +1,41 @@
+/** require dependencies */
+const express = require('express');
+const routes = require('./routes/');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const router = express.Router();
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/medium';
+
+/** connect to MongoDB datastore */
+try {
+  mongoose.connect(
+    url,
+    { useNewUrlParser: true }
+  );
+} catch (error) {}
+
+let port = process.env.PORT;
+
+/** set up routes {API Endpoints} */
+routes(router);
+
+/** set up middlewares */
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet());
+//app.use('/static',express.static(path.join(__dirname,'static')))
+
+// to use with a prefix
+app.use('/api', router);
+app.use(router);
+
+/** start server */
+app.listen(port, () => {
+  console.log(`Server started at port: ${port}`);
+});
