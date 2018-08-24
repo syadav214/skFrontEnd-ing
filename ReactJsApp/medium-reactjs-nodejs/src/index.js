ac810ed67df9339dbe0a12e6f4cf9f27ff687203
Santoshkumar Yadav <syadav214@gmail.com>
2018-08-24 15:24:35 +0530

    added routes

--- ReactJsApp/medium-reactjs-nodejs/src/index.js
+++ ReactJsApp/medium-reactjs-nodejs/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/medium.css';
 import { Provider } from 'react-redux';
-
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import App from './App.js';
 import registerServiceWorker from './registerServiceWorker';
 import { store, history } from './redux/store';
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
