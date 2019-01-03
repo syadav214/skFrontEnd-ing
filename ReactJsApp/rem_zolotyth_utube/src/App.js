efa6c20f1733a0c17faa1df316a0d2d3e44e7353
Santoshkumar Yadav <syadav214@gmail.com>
2019-01-03 17:41:43 +0530

    added semantic ui

--- ReactJsApp/rem_zolotyth_utube/src/App.js
+++ ReactJsApp/rem_zolotyth_utube/src/App.js
@@ -3,8 +3,11 @@ import { Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 
+/* Default behavior of semantic-ui that it does not have margin.
+We can get margin if we add class name 'ui container'
+*/
 const App = () => (
-  <div>
+  <div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
   </div>
