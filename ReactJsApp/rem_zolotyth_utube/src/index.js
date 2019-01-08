924c0a7e2080d5b3f115c0f7d0d59f5395f6b06c
Santoshkumar Yadav <syadav214@gmail.com>
2019-01-08 17:56:52 +0530

    reducer basic

--- ReactJsApp/rem_zolotyth_utube/src/index.js
+++ ReactJsApp/rem_zolotyth_utube/src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+
+/* For a normal form submit, we should use default state object. 
+And for storage across application, we should use redux */
+
+import {createStore,applyMiddleware} from 'redux';
+import {Provider} from 'react-redux'; // to connect react and redux
+import thunk from 'redux-thunk'; // return a function instead of action; The inner function receives the store methods dispatch and getState as parameters.
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from '.rootReducer'; // reducer takes state and give new state
+
+
+
+const store =  createStore (
+  rootReducer, applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <BrowserRouter>
