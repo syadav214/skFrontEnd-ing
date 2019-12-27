154cd5c666a38c2a70525d5275dc136d99541fae
Santoshkumar Yadav <syadav214@gmail.com>
2019-12-27 14:46:08 +0530

    added tick  tac toe

---
+++ ReactJsApp/tic-tac-toe/src/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currSign: '' };
+  }
+
+  onBtnClick() {
+    if (this.state.currSign === '') {
+      this.setState({ currSign: this.props.currSign }, () => {
+        let newSign = 'X';
+        if (this.props.currSign === 'X') {
+          newSign = 'O';
+        }
+        this.props.changeSign(newSign);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <button
+        onClick={this.onBtnClick.bind(this)}
+        className='square'
+        style={{ width: '40px', height: '40px' }}
+      >
+        {this.state.currSign}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = { currSign: 'X' };
+  }
+
+  changeSign(newSign) {
+    this.setState({ currSign: newSign });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        currSign={this.state.currSign}
+        changeSign={this.changeSign.bind(this)}
+      />
+    );
+  }
+
+  reset() {
+    this.setState({ currSign: 'X' });
+  }
+
+  render() {
+    const status = 'Next player: ' + this.state.currSign;
+
+    return (
+      <div>
+        <div className='status'>{status}</div>
+        <button onClick={this.reset.bind(this)}>Reset</button>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className='game'>
+        <div className='game-board'>
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById('root'));
