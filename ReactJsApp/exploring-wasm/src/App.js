b846302c3aa5c6a5fc173d8b62c0b85088f8db6b
Santoshkumar Yadav <syadav214@gmail.com>
2020-12-28 13:37:50 +0530

    wasm testing

---
+++ ReactJsApp/exploring-wasm/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+function App() {
+  return (
+    <React.Fragment>
+      helllo
+      <br />
+      <Button />
+    </React.Fragment>
+  );
+}
+
+export default App;
